b887ee005c83c0722c92873c42978580f1cca416
qirsam <qirsam@gmai.com>
2022-09-14 05:49:46 +0300

    tests for BookControllerIT

--- src/main/java/com/qirsam/mini_library/dto/BookCreateUpdateDto.java
+++ src/main/java/com/qirsam/mini_library/dto/BookCreateUpdateDto.java
@@ -2,8 +2,10 @@ package com.qirsam.mini_library.dto;
 
 import com.qirsam.mini_library.database.entity.library.Genre;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
 
 @Value
+@FieldNameConstants
 public class BookCreateUpdateDto {
     String title;
     Integer authorId;
