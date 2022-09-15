f9c3077244ae53cd24386d4bb917e806008d87b3
qirsam <qirsam@gmai.com>
2022-09-15 18:01:29 +0300

    test build 1.0.1

--- src/main/java/com/qirsam/mini_library/dto/AuthorReadDto.java
+++ src/main/java/com/qirsam/mini_library/dto/AuthorReadDto.java
@@ -2,10 +2,13 @@ package com.qirsam.mini_library.dto;
 
 import lombok.Value;
 
+import java.time.LocalDate;
+
 @Value
 public class AuthorReadDto {
     Integer id;
     String firstname;
     String lastname;
+    LocalDate birthDate;
     String description;
 }
