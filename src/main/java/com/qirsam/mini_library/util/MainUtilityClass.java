417f3656631dbba65cff71bf2e5dac69f4698092
qirsam <qirsam@gmail.com>
2022-11-21 20:55:44 +0300

    front /books #3

--- src/main/java/com/qirsam/mini_library/util/MainUtilityClass.java
+++ src/main/java/com/qirsam/mini_library/util/MainUtilityClass.java
@@ -9,7 +9,7 @@ public class MainUtilityClass {
     public static final int FIRST_PAGE = 1;
     public static final String YA_DISK_APP_PATH_IMAGE = "app:/images/";
     public static PageRequest defaultPageRequest(int pageNumber) {
-        return PageRequest.of(pageNumber - 1, 5);
+        return PageRequest.of(pageNumber - 1, 20);
     }
 
 
